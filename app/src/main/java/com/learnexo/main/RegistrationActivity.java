59e7fc1b08be29edce24a146b19d0d85aac7fb16
Thriinadh <3thri.reddy>
2018-05-17 12:54:09 +0530

    added crack icon in activity_full_answer

--- app/src/main/java/com/learnexo/main/RegistrationActivity.java
+++ app/src/main/java/com/learnexo/main/RegistrationActivity.java
@@ -56,9 +56,7 @@ public class RegistrationActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+
                 finish();
                 return true;
 
