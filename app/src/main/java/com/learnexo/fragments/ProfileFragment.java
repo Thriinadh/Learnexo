a6242b06287a5da689a1bc2a9e14545045679d16
Thriinadh <3thri.reddy>
2018-05-22 15:27:51 +0530

    profile

--- app/src/main/java/com/learnexo/fragments/ProfileFragment.java
+++ app/src/main/java/com/learnexo/fragments/ProfileFragment.java
@@ -1,20 +1,29 @@
 package com.learnexo.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.learnexo.main.R;
+import com.learnexo.util.FirebaseUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +31,16 @@ import java.util.List;
 public class ProfileFragment extends Fragment {
 
     private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private FrameLayout frameLayout;
+    private CircleImageView profileImage;
+    private TextView userName;
+    private TextView description;
+
+    private String mUserId;
+    public static String sDpUrl;
+    public static String sName;
+
+    FirebaseUtil mFirebaseUtil = new FirebaseUtil();
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -40,52 +58,114 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        viewPager = view.findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
+        profileImage = view.findViewById(R.id.profile_image);
+        userName = view.findViewById(R.id.user_name);
+        description = view.findViewById(R.id.description);
+        frameLayout = view.findViewById(R.id.fragment_container);
 
         tabLayout = view.findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addTab(tabLayout.newTab().setText("Posts"));
+        tabLayout.addTab(tabLayout.newTab().setText("Answers"));
+        tabLayout.addTab(tabLayout.newTab().setText("Questions"));
+        tabLayout.addTab(tabLayout.newTab().setText("Interests"));
+
+        if (savedInstanceState == null) {
+            getChildFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new ProfilePostsFragment())
+                    .commit();
+        }
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            Fragment fragment;
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tabLayout.getSelectedTabPosition() == 0){
+                  //  tabLayout.setTabTextColors(Color.parseColor("#1da1f2"));
+                    fragment = new ProfilePostsFragment();
+                    replaceFragment(fragment);
+
+                }else if(tabLayout.getSelectedTabPosition() == 1){
+
+                    fragment = new ProfileAnswers();
+                    replaceFragment(fragment);
+
+                }else if(tabLayout.getSelectedTabPosition() == 2){
+
+                    fragment = new ProfileQuestions();
+                    replaceFragment(fragment);
+
+                }else if(tabLayout.getSelectedTabPosition() == 3){
+
+                    fragment = new ProfileInterests();
+                    replaceFragment(fragment);
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        getDPandUserName();
 
         // Inflate the layout for this fragment
         return view;
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
-        adapter.addFragment(new ProfilePostsFragment(), "Posts");
-        adapter.addFragment(new ProfileAnswers(), "Answers");
-        adapter.addFragment(new ProfileQuestions(), "Questions");
-        adapter.addFragment(new ProfileInterests(), "Interests");
-        viewPager.setAdapter(adapter);
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
+    private void getDPandUserName() {
+        mUserId = FirebaseUtil.getCurrentUserId();
+        mFirebaseUtil.mFirestore.collection("users").document(mUserId).
+                collection("reg_details").document("doc").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
+                if(task.isSuccessful()) {
+                    DocumentSnapshot snapshot = task.getResult();
+                    if(snapshot.exists()) {
+                        String firstName = snapshot.getString("firstName");
+                        String lastName = snapshot.getString("lastName");
 
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
+                        sName =firstName.concat(" "+lastName);
+                        userName.setText(sName);
 
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
+                        String descriptionn = snapshot.getString("description");
+                        description.setText(descriptionn);
 
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
+                        RequestOptions placeholderRequest = new RequestOptions();
+                        placeholderRequest.diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .placeholder(R.drawable.empty_profilee);
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+                        sDpUrl =  snapshot.getString("dpUrl");
+
+                        if (sDpUrl !=null&&null!=getActivity()) {
+                            Glide.with(getActivity().getApplicationContext()).load(sDpUrl).apply(placeholderRequest).into(profileImage);
+                        }
+                    }
+                } else {
+
+                    String error = task.getException().getMessage();
+                    Toast.makeText(getActivity(), "Firestore Retrieve Error : " + error, Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
     }
 
 }
