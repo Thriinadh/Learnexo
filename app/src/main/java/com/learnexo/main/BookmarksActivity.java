3476e2c02a0b8b8b3050e597520684b482bad225
GN Reddy <thgopiinadh@gmail.com>
2018-05-31 19:35:07 +0530

    implemented bookmarks

--- app/src/main/java/com/learnexo/main/BookmarksActivity.java
+++ app/src/main/java/com/learnexo/main/BookmarksActivity.java
@@ -1,6 +1,5 @@
 package com.learnexo.main;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -42,18 +41,13 @@ public class BookmarksActivity extends AppCompatActivity {
         feedRecyclerView.setLayoutManager(new LinearLayoutManager(BookmarksActivity.this));
         feedRecyclerView.setAdapter(mAdapter);
 
-        new BookMarksFetcher().execute(FirebaseUtil.getCurrentUserId());
+        fetchBookMarks(FirebaseUtil.getCurrentUserId());
+
 
     }
 
+        private void fetchBookMarks(String currentUserId) {
 
-
-
-    public class BookMarksFetcher extends AsyncTask<String, Void, List<FeedItem>>{
-
-        @Override
-        protected List<FeedItem> doInBackground(String... strings) {
-            String currentUserId = strings[0];
             mFirebaseUtil.mFirestore.collection("users").document(currentUserId).collection("bookmarks").
                     get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                 @Override
@@ -65,62 +59,41 @@ public class BookmarksActivity extends AppCompatActivity {
                         String bookMarkItemId = bookmark.getBookMarkItemId();
                         String publisherId = bookmark.getPublisherId();
 
-                        if (bookmark.getBookMarkType().name().equals("POST"))
-                            new FeedItemFetcher().execute(publisherId,bookMarkItemId,"posts");
-                        else
-                            new FeedItemFetcher().execute(publisherId,bookMarkItemId,"answers");
+                                if (bookmark.getBookMarkType().name().equals("POST")) {
+
+                                    mFirebaseUtil.mFirestore.collection("users").document(publisherId).
+                                            collection("posts").document(bookMarkItemId).get().addOnSuccessListener(
+                                            new OnSuccessListener<DocumentSnapshot>() {
+                                                @Override
+                                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                    Post post = documentSnapshot.toObject(Post.class);
+                                                    mFeedItems.add(post);
+                                                    mAdapter.notifyDataSetChanged();
+                                                }
+                                            });
+                                }
+                                else {
+                                    mFirebaseUtil.mFirestore.collection("users").document(publisherId).
+                                            collection("answers").document(bookMarkItemId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                            Answer answer = documentSnapshot.toObject(Answer.class);
+                                            mFeedItems.add(answer);
+                                            mAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+                                }
+
+
+
+
                     }
 
                 }
             });
 
-
-            return null;
-        }
     }
 
-
-
-
-
-    public class FeedItemFetcher extends AsyncTask<String, Void, FeedItem>{
-
-        @Override
-        protected FeedItem doInBackground(String... strings) {
-            String publisherId = strings[0];
-            String feedItemId = strings[1];
-            final String path = strings[2];
-            final FeedItem[] feedItem = new FeedItem[1];
-
-            mFirebaseUtil.mFirestore.collection("users").document(publisherId).collection(path).document(feedItemId)
-                    .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    if(path.equals("posts")){
-                        feedItem[0] =documentSnapshot.toObject(Post.class);
-                    }else{
-                        feedItem[0]=(documentSnapshot.toObject(Answer.class));
-                    }
-                }
-            });
-
-            return feedItem[0];
-        }
-
-
-        @Override
-        protected void onPostExecute(FeedItem feedItem) {
-            super.onPostExecute(feedItem);
-
-            notifyAdapter(feedItem);
-        }
-    }
-
-    private void notifyAdapter(FeedItem feedItem) {
-        if(feedItem!=null)
-        mFeedItems.add(feedItem);
-        mAdapter.notifyDataSetChanged();
-    }
 
 
 }
