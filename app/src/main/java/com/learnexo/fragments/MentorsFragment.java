cf2719a58397d01e6b073701b00227ce3e9eae10
Thriinadh <3thri.reddy>
2018-06-03 11:51:13 +0530

    MentorsFragment

--- app/src/main/java/com/learnexo/fragments/MentorsFragment.java
+++ app/src/main/java/com/learnexo/fragments/MentorsFragment.java
@@ -1,17 +1,28 @@
 package com.learnexo.fragments;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.learnexo.main.R;
+import com.learnexo.model.connect.LearnerOptions;
+import com.learnexo.util.DummyData;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.List;
+
+import static com.learnexo.model.connect.LearnerOptions.LINE_TYPE;
+import static com.learnexo.model.connect.LearnerOptions.OPTION_TYPE;
+
 public class MentorsFragment extends Fragment {
 
 
@@ -24,8 +35,95 @@ public class MentorsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mentors, container, false);
+
+        RecyclerView learnersRecycler = view.findViewById(R.id.mentorsRecyclerView);
+        learnersRecycler.setHasFixedSize(true);
+        DifferentRowAdapter differentRowAdapter = new DifferentRowAdapter(DummyData.getData());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        learnersRecycler.setLayoutManager(layoutManager);
+        learnersRecycler.setAdapter(differentRowAdapter);
+
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public class DifferentRowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private List<LearnerOptions> mList;
+        public DifferentRowAdapter(List<LearnerOptions> list) {
+            this.mList = list;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view;
+            switch (viewType) {
+                case OPTION_TYPE:
+                    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.learners_items, parent, false);
+                    return new LearnersViewHolder(view);
+                case LINE_TYPE:
+                    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.line_separator, parent, false);
+                    return new LineViewHolder(view);
+            }
+            return null;
+        }
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            LearnerOptions object = mList.get(position);
+            if (object != null) {
+                switch (object.getmOptionType()) {
+                    case OPTION_TYPE:
+                        ((LearnersViewHolder) holder).mTitle.setText(object.getmOption());
+
+                        String uri="@drawable/connect_icon";
+                        if(null != object.getmOptionIcon())
+                            uri = "@drawable/"+object.getmOptionIcon();  // where myresource (without the extension) is the file
+
+                        int imageResource = getResources().getIdentifier(uri, "drawable", getActivity().getPackageName());
+                        Drawable res = getResources().getDrawable(imageResource);
+                        ((LearnersViewHolder) holder).imageView.setImageDrawable(res);
+                        break;
+                    case LINE_TYPE:
+                        ((LineViewHolder) holder).mLineView.setBackgroundColor(Color.parseColor("#1da1f2"));
+                        break;
+                }
+            }
+        }
+        @Override
+        public int getItemCount() {
+            if (mList == null)
+                return 0;
+            return mList.size();
+        }
+        @Override
+        public int getItemViewType(int position) {
+            if (mList != null) {
+                LearnerOptions object = mList.get(position);
+                if (object != null) {
+                    return object.getmOptionType();
+                }
+            }
+            return 0;
+        }
+        public class LearnersViewHolder extends RecyclerView.ViewHolder {
+            private TextView mTitle;
+            private ImageView imageView;
+            // private View mLineView;
+            public LearnersViewHolder(View itemView) {
+                super(itemView);
+                mTitle = itemView.findViewById(R.id.titleTextView);
+                imageView = itemView.findViewById(R.id.iconImage);
+                // mLineView = itemView.findViewById(R.id.lineSeparator);
+            }
+        }
+        public class LineViewHolder extends RecyclerView.ViewHolder {
+            private View mLineView;
+            public LineViewHolder(View itemView) {
+                super(itemView);
+                mLineView = itemView.findViewById(R.id.lineSeparator);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/learnexo/util/DummyData.java
+++ app/src/main/java/com/learnexo/util/DummyData.java
@@ -15,6 +15,16 @@ public final class DummyData {
         list.add(new LearnerOptions("Get career guidance", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_24px"));
         list.add(new LearnerOptions("Get Project guidance", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_people_24px"));
         list.add(new LearnerOptions("Get research guidance", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_add_24px"));
+        list.add(new LearnerOptions(null, null, LearnerOptions.LINE_TYPE, null));
+        list.add(new LearnerOptions("Get Referred", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_24px"));
+        list.add(new LearnerOptions("Do Industry Work", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_people_24px"));
+        list.add(new LearnerOptions("Become Freelancer", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_add_24px"));
+        list.add(new LearnerOptions("Do Research", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_people_24px"));
+        list.add(new LearnerOptions("Apply For Internship", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_add_24px"));
+        list.add(new LearnerOptions(null, null, LearnerOptions.LINE_TYPE, null));
+        list.add(new LearnerOptions("Get Project Funding", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_person_24px"));
+        list.add(new LearnerOptions("Get Research Funding", "GetGuidanceActivity", LearnerOptions.OPTION_TYPE, "ic_twotone_people_24px"));
+
 //        list.add(new LearnerOptions("Groups", "GroupActivity", LearnerOptions.OPTION_TYPE));
 //        list.add(new LearnerOptions("Groups", "GroupActivity", LearnerOptions.OPTION_TYPE));
 //        list.add(new LearnerOptions("Groups", "GroupActivity", LearnerOptions.OPTION_TYPE));
